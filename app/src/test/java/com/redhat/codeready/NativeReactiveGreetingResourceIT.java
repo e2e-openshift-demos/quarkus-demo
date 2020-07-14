9e15d214f81812002987c1b35b707fbf617db9a8
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2020-07-14 18:26:40 +0300

    step-01 initial commit

---
+++ app/src/test/java/com/redhat/codeready/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.codeready;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
