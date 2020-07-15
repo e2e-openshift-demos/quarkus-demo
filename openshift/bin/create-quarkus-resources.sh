e2f4276e2b0429797b6a710642dbac6e13c578ae
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2020-07-15 16:53:56 +0300

    ajdust configmap key name

--- openshift/bin/create-quarkus-resources.sh
+++ openshift/bin/create-quarkus-resources.sh
@@ -9,4 +9,4 @@ export APP_NS=CURRENT
 
 SECRET_FILE_NAME=ca.pem bash -x $BASE_DIR/utils/create-secret.sh custom-ca $BASE_DIR/certs/custom-ca.pem
 SECRET_FILE_NAME=server.pfx bash -x $BASE_DIR/utils/create-secret.sh quarkus-demo-certs $BASE_DIR/certs/quarkus-demo.pfx
-bash -x $BASE_DIR/utils/create-cm.sh quarkus-application-config $BASE_DIR/app/src/main/resources/application.properties
+CONFIG_FILE_NAME=application.properties bash -x $BASE_DIR/utils/create-cm.sh quarkus-application-config $BASE_DIR/app/src/main/resources/prod-application.properties
